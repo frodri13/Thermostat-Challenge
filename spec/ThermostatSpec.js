be981596a3838e08c5144c8901367ab6b02007e6
Fabio <alessf13@gmail.com>
2021-07-06 15:47:32 +0200

    Refactoring and files deleted

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -4,6 +4,6 @@ describe("Thermostat", function () {
   it("has an initial temperature of 20 degrees", function () {
     //
     thermostat = new Thermostat();
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
 });
